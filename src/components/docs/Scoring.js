f7c7e193e61febd9e5d95a743e5839a0cd869ba4
Jeppe <jeppeqin@gmail.com>
2022-02-25 15:14:50 +0100

    scoring

--- src/components/docs/Scoring.js
+++ src/components/docs/Scoring.js
@@ -41,34 +41,64 @@ function Scoring() {
         <br />
 
         <Box>
-          <Typography color='primary' sx={{ mb: '5px' }}>
+          <Typography color='rgba(255, 255, 255, 0.5)' sx={{ mb: '5px' }}>
             Example of scoring:
           </Typography>
 
-          <Typography color='primary'>
+          <Typography color='rgba(255, 255, 255, 0.5)'>
             Player A owns 0.1 ETH and 20 MATIC
           </Typography>
 
-          <Typography color='primary' sx={{ mb: '5px' }}>
+          <Typography color='rgba(255, 255, 255, 0.5)' sx={{ mb: '5px' }}>
             Player B owns 0.3 ETH and 10 MATIC
           </Typography>
 
-          <Typography color='primary'>
+          <Typography color='rgba(255, 255, 255, 0.5)'>
             Total pool of 0.4 ETH gets liquidated to 1000 DAI
           </Typography>
 
-          <Typography color='primary' sx={{ mb: '5px' }}>
+          <Typography color='rgba(255, 255, 255, 0.5)' sx={{ mb: '5px' }}>
             Total pool of 30 MATIC gets liquidated to 60 DAI
           </Typography>
 
-          <Typography color='primary'>
+          <Typography color='rgba(255, 255, 255, 0.5)'>
             Player A final score: 0.1 ETH / 0.4 ETH * 1000 + 20 MATIC / 30 MATIC * 60 = 290
           </Typography>
 
-          <Typography color='primary'>
+          <Typography color='rgba(255, 255, 255, 0.5)'>
             Player B final score: 0.3 ETH / 0.4 ETH * 1000 + 10 MATIC / 30 MATIC * 60 = 770
           </Typography>
         </Box>
+      </Box>
+
+      <Box mt={3}>
+        <Typography color='primary' variant='h6'>
+          Prize pool and distribution
+        </Typography>
+      </Box>
+
+      <Box mt={1}>
+        <Typography>
+          The prize pool is the total amount liquidated minus a 10% protocol fee.
+          In the above example, the prize pool would be 954 DAI.
+          Below you'll get an overview of prize distribution.
+        </Typography>
+      </Box>
+
+      <Box mt={3}>
+        <Typography color='secondary' variant='h6'>
+          standard distribution
+        </Typography>
+      </Box>
+
+      <Box mt={1}>
+        <Typography>
+          50% of the total pool is used to refund players their entry fee, starting from the winner and continuing down the ranks until the pool is empty.
+        </Typography>
+        <br />
+        <Typography>
+          The rest of the prize pool is awarded to the top 20%, distributed proportional to the players' final positions.
+        </Typography>
       </Box>
 
     </Box>
